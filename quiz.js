77f4fdb2636919973e38376e4caeb39ade4e1eb7
jws1998 <jws_1998@outlook.com>
2021-04-27 14:04:59 +0800

    Worked on developing the quiz

--- quiz.js
+++ quiz.js
@@ -1,67 +1,9 @@
-// select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
-
-// create our questions
-let questions = [
-    {
-        question : "What is the lightest element on earth?",
-        choiceA : "Hydrogen",
-        choiceB : "Helium",
-        choiceC : "Oxygen",
-        correct : "A"
-    },{
-        question : "What combines with hydrogen to create water?",
-        choiceA : "Helium",
-        choiceB : "Oxygen",
-        choiceC : "Air",
-        correct : "B"
-    },{
-        question : "Hydrogen is made up of one _____ and one _____ .",
-        choiceA : "Helium and Oxygen",
-        choiceB : "Atom and Molecule",
-        choiceC : "Proton and Electron",
-        correct : "C"
-    }
-];
-
-// create some variables
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-let score = 0;
-
-// render a question
-function renderQuestion(){
-    let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
-
-start.addEventListener("click",startQuiz);
-
-// start quiz
+const startbutton = document.getElementById('start')
 var factsTitle = document.getElementById('id');
 var factsList = document.getElementById('id1');
 var clickWhenReady = document.getElementById('id2');
 var table = document.getElementById("id3");
+var quiz = document.getElementById("quiz");
 
 function startQuiz(){
     factsTitle.remove();
@@ -69,90 +11,73 @@ function startQuiz(){
     clickWhenReady.remove();
     table.remove();
     start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    quiz.style = "display";
+    displayQuestion();
 }
 
-// render progress
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+let options = [
+    {
+        question: "What is the lightest elemnt on earth?",
+        choiceA : "Hydrogen",
+        choiceB : "Helium",
+        choiceC : "Oxygen",
+        correct : "Hydrogen"
+    },{
+        question : "What combines with hydrogen to create water?",
+        choiceA : "Helium",
+        choiceB : "Oxygen",
+        choiceC : "Air",
+        correct : "B"
+    }
+];
+
+const question = document.getElementById('question');
+const choiceA = document.getElementById('A');
+const choiceB = document.getElementById('B');
+const choiceC = document.getElementById('C');
+const score = document.getElementById("score");
+
+// question counter 
+let x = 0;
+
+function displayQuestion(){ 
+    questionNum.innerHTML = "Question";
+    question.innerHTML = options[x].question;
+    choiceA.innerHTML = options[x].choiceA;
+    choiceB.innerHTML = options[x].choiceB;
+    choiceC.innerHTML = options[x].choiceC;
+}
+
+// score counter
+let grade = 0;
+
+// Checking the To see if the answer is correct.
+function checkAnswerA(){
+    if(options[0].choiceA == options[0].correct){
+        grade = grade +1
+        score.innerHTML = grade;
+    }
+    else{
+        score.innerHTML = grade
     }
 }
 
-// counter render
-
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
+function checkAnswerB(){
+    if(options[0].choiceB == options[0].correct){
+        grade = grade +1
+        score.innerHTML = grade;
+    }
+    else{
+        score.innerHTML = grade
+    }
+}
+function checkAnswerC(){
+    if(options[0].choiceC == options[0].correct){
+        grade = grade + 1
+        score.innerHTML = grade;
+    }
+    else{
+        score.innerHTML = grade
     }
 }
 
-// checkAnwer
-
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    }else{
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
-    }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }else{
-        // end the quiz and show the score
-        clearInterval(TIMER);
-        scoreRender();
-    }
-}
-
-// answer is correct
-function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-
-// answer is Wrong
-function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-}
-
-// score render
-function scoreRender(){
-    scoreDiv.style.display = "block";
-    
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
-}
