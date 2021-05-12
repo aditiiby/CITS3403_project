2e644d2fa54e97f06039526c50d09ddf2d16eeab
jws1998 <jws_1998@outlook.com>
2021-05-12 11:10:55 +0800

    adjusted the connection with each of the pages

--- app.py
+++ app.py
@@ -13,6 +13,30 @@ def index():
 def lessons():
     return render_template('lessons.html')
 
+@app.route('/Hydrogen')
+def Hydrogen ():
+    return render_template('Hydrogen.html')
+
+@app.route('/carbon')
+def carbon():
+    return render_template('carbon.html')
+
+@app.route('/helium')
+def helium():
+    return render_template('helium.html')
+
+@app.route('/iron')
+def iron():
+    return render_template('iron.html')
+
+@app.route('/nitrogen')
+def nitrogen():
+    return render_template('nitrogen.html')
+
+@app.route('/oxygen')
+def oxygen():
+    return render_template('oxygen.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -20,6 +44,10 @@ def about():
 @app.route('/nav')
 def nav():
     return render_template('nav.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
 
