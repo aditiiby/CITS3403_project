2853b8bdd1b127a7791b5ef67172450960f5e472
vwojcieski <victor.wojcieski4@gmail.com>
2021-05-14 13:29:07 +0800

    implemented a database

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
+import sqlalchemy
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Email, Length
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy  import SQLAlchemy
+
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SuperSecretKey123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(30))
+    email = db.Column(db.String(60), unique=True)
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=8, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
-    
-
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=30)])
 
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=60)])
     username = StringField('username', validators=[InputRequired(), Length(min=8, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=30)])
 
 
 @app.route('/')
@@ -68,7 +79,7 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        print("+++++++++++++++++++++++++")
+ 
         return '<h1>' + form.username.data +'</h1>'
 
     return render_template('login.html', form = form)
@@ -78,8 +89,11 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        print("+++++++++++++++++++++++++")
-        return '<h1>' + form.username.data +'</h1>'
+        newUser = User(username = form.username.data, password = form.password.data, email = form.email .data)
+        db.session.add(newUser)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'#return profile page
 
     return render_template('signup.html', form = form)
 
