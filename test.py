b0e93085fdd21852cdb8238d30e1388719ffb703
jws1998 <jws_1998@outlook.com>
2021-05-17 11:54:16 +0800

    nothing

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class UserModelTest(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_password_hashing(self):
+    def test_password(self):
         u = User(username ='james123', password="james123")
         self.assertFalse(u.login('josh'))
         self.assertTrue(u.login('james123'))
