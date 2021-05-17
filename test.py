5b7240cb3fea033903274605826706cd916c72a8
jws1998 <jws_1998@outlook.com>
2021-05-17 12:45:51 +0800

    adjust the test_results funciton

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class scoreModelTest(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
         db.create_all()
-        user1 = User(username="Testcase", password="kasjdnfkasj", email="example@gmail.com", carbonResults=50)
+        user1 = User(username="Testcase", password="kasjdnfkasj", email="example@gmail.com", hydrogenResults = 50, heliumResults= 50, carbonResults=50, nitrogenResults=50, oxygenResults=50, ironResults=50)
         db.session.add(user1)
         db.session.commit()
 
@@ -15,8 +15,19 @@ class scoreModelTest(unittest.TestCase):
 
     def test_result(self):
         user1 = User.query.filter_by(username="Testcase").first()
+        self.assertFalse(user1.hydrogenResults == 10)
+        self.assertFalse(user1.heliumResults == 10)
         self.assertFalse(user1.carbonResults == 10)
-        self.assertTrue(user1.carbonResults == 60)
+        self.assertFalse(user1.nitrogenResults == 10)
+        self.assertFalse(user1.oxygenResults == 10)
+        self.assertFalse(user1.ironResults == 10)
+        self.assertTrue(user1.hydrogenResults == 50)
+        self.assertTrue(user1.heliumResults == 50)
+        self.assertTrue(user1.carbonResults == 50)
+        self.assertTrue(user1.nitrogenResults == 50)
+        self.assertTrue(user1.oxygenResults == 50)
+        self.assertTrue(user1.ironResults == 50)
+
 
 if __name__=='__main__':
     unittest.main(verbosity = 2)
